d67dabe369b07fdd8b36214b8e2e7de535a0896b
Taufiq63569 <taufiq63569@gmail.com>
2022-02-19 22:02:00 +0600

    splash screen fully complete

--- lib/screens/splash_screen/splash_screen_class.dart
+++ lib/screens/splash_screen/splash_screen_class.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:qrcode_scanner/components/custom_text_style/my_text_style_class.dart';
+import 'package:qrcode_scanner/screens/option_section/option_class.dart';
 import 'package:qrcode_scanner/utils/colors.dart';
 import 'package:qrcode_scanner/utils/strings.dart';
 
@@ -14,13 +17,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Timer(Duration(seconds: 2), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (builder)=> const Options()));
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyColors.backColor,
+      backgroundColor: MyColors.primaryColor,
       appBar: AppBar(
         systemOverlayStyle: const SystemUiOverlayStyle(
-            systemNavigationBarColor: MyColors.backColor),
-        backgroundColor: MyColors.backColor,
+            systemNavigationBarColor: MyColors.primaryColor),
+        backgroundColor: MyColors.primaryColor,
         elevation: 0,
         //title: Text(Texts.qrCodeScanner, style: MyTextStyle.mediumStyle(),),
         centerTitle: true,
@@ -30,19 +40,27 @@ class _SplashScreenState extends State<SplashScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Icon(
-              FontAwesomeIcons.qrcode,
-              size: 100,
-              color: MyColors.primaryColor,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  FontAwesomeIcons.qrcode,
+                  size: 30,
+                  color: MyColors.backColor,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  Texts.qrCodeScanner,
+                  style: MyTextStyle.mediumStyle(fontSize: 25, fontColor: MyColors.backColor),
+                ),
+
+              ],
             ),
             const SizedBox(height: 50),
-            Text(
-              Texts.qrCodeScanner,
-              style: MyTextStyle.mediumStyle(fontSize: 45),
-            ),
-            const SizedBox(height: 35),
-            CircularProgressIndicator(
-              color: MyColors.primaryColor,
+
+
+            const CircularProgressIndicator(
+              color: MyColors.backColor,
               strokeWidth: 20,
             ),
           ],
